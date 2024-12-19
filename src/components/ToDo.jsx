91a2fadf89f4d2d86d117ed13e330a6aa5c64fa6
harshit <harshitmahara20@gmail.com>
2024-12-19 21:50:38 +0530

    add and delete

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,33 +1,72 @@
-import React from 'react'
-import todo_icon from '../assets/todo_icon.png'
-import Todoitems from './Todoitems'
+import React, { useRef, useState } from 'react';
+import todo_icon from '../assets/todo_icon.png';
+import Todoitems from './Todoitems';
 
 const ToDo = () => {
+
+  const [todoList , setToDoList] = useState([])
+
+
+
+  const inputRef = useRef(); // Create a ref for the input element
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim(); // Access the input value
+
+    if(inputText=== ""){
+      return null ;  
+    }
+
+
+
+    const newTodo = {
+      id : Date.now(),
+      text :inputText,
+      isComplete : false 
+    }
+    setToDoList((prev)=>[...prev, newTodo]);
+    inputRef.current.value = "";
+     
+
+  };
+  const deleteTodo =(id)=>{
+    setToDoList((prvTodos)=>{
+      return prvTodos.filter((todo)=> todo.id !==id)
+    })
+  }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-        <div className='flex items-center mt-7 gap-2'>
-            <img className='w-8' src={todo_icon}></img>
+      <div className='flex items-center mt-7 gap-2'>
+        <img className='w-8' src={todo_icon} alt="ToDo Icon" />
+        <h1 className='text-3xl font-semibold'>To Do List</h1>
+      </div>
 
-            <h1 className='text-3xl font-semibold '>
-                To Do List 
-            </h1>
-            
-        </div>
-        <div className='flex items-center my-7 bg-gray-200 rounded-full '>
-                <input className='bg-transparent border-0 outline-none 
-                flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='add the task'></input>
-                <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-            </div>
+      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        {/* Attach the ref to the input element */}
+        <input
+          ref={inputRef}
+          className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
+          type='text'
+          placeholder='Add the task'
+        />
+        <button
+          onClick={add}
+          className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'
+        >
+          ADD +
+        </button>
+      </div>
 
-       {/* ------todo list ------*/}
-       <div>
-        <Todoitems text={"create ui"}/>
-        <Todoitems text= {"learn react "}/>
+      {/* To-do list items */}
+      <div>
+        {todoList.map((item,index)=>{
+          return <Todoitems key ={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo ={deleteTodo } />
+        })}
 
-       </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ToDo
+export default ToDo;
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -4,7 +4,7 @@ import delete_icon from '../assets/delete.png'
 
 
 
-const Todoitems = ({text}) => {
+const Todoitems = ({text ,id, isComplete,deleteTodo}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
@@ -12,7 +12,7 @@ const Todoitems = ({text}) => {
             <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
         </div>
 
-        <img src={delete_icon} className='w-3.5 cursor-pointer'></img>
+        <img onClick={()=>{deleteTodo(id)}} src={delete_icon} className='w-3.5 cursor-pointer'></img>
       
     </div>
   )
